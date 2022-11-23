85bd5a29c0030f1a8577cd6319c65e6c6a334bdc
IMakeBotsForYou <40301997+IMakeBotsForYou@users.noreply.github.com>
2022-11-23 15:51:04 +0200

    Fixed the JP range

--- helpers.py
+++ helpers.py
@@ -11,20 +11,31 @@ from discord.ext.commands import Context
 # ディスコードでは、絵文字は 「 <a:名前:ID 」 もしくは 「 <:名前:ID 」
 # のように表示される。　GIFの場合はAnimationのAがつく
 find_emoji = compile(r'(?:<a:)|(?:<:)\w+:\d+>')
+
 # 英語のアルファベット
 find_english = compile(r"[a-zA-Z]")
-# 日本語のひらがな、カタカナ、半角カタカナ、特別キャラクター、そして漢字
-find_japanese = compile(r"[あ-んア-ン぀-ゟ゠-ヿ＀-￯]")
-# 日本語と英語をあわせたフィルター
-find_english_japanese = compile(r"[a-zA-Zあ-んア-ン぀-ゟ゠-ヿ＀-￯]")
+
+# 日本語のひらがな、カタカナ、特別キャラクター、そして漢字
+# http://www.rikai.com/library/kanjitables/kanji_codes.unicode.shtml
+# Puncuation "、-〽"
+# Hiragana "ぁ-ゟ"
+# Katakana "゠-ヿ"
+# Halfwidth Katana & more = "｡- ﾟ"
+# CJK unifed ideographs "一-齢"
+find_japanese = compile(r"[、-〽ぁ-ゟ゠-ヿ｡-ﾟ一-齢]")
+
+
 # 特別キャラクター
-find_characters = compile(r"[0-9?><;,.{}[\]\-_+=!@#$%\^&*|'、ー　-〿 ]")
+find_characters = compile(r"[0-9?><;,.{}[\]\-_+=!@#$%\^&*|'、ー ]")
+
 # リンクのフィルター。　http, あるいは https のあとにどんな文字が付こうが入る
 find_links = compile(r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+")
 
+# ネタベレメッセージ、隠れたメッセージ
+find_spoilers = compile(r"\|\|.+?\|\|")
+
+
 def fix_channel_name(channel_name: str) -> str:
-    # all_ch = find_english_japanese.findall(channel_name)
-    # return "".join(all_ch)
     return channel_name[1:]
 
 def get_channel_id(channel_name: str) -> str:
@@ -43,10 +54,7 @@ def load_json(json_path: str) -> json:
 config = load_json("config.json")
 owners = config["owners"]
 
-
-
-def log(message: discord.Message, spacing="\n") -> None:
-    print(spacing)
+def calculate_percentages(message: discord.Message) -> tuple:
     # Filter through stuff that isn't actually characters
     content = message.content
     
@@ -57,15 +65,16 @@ def log(message: discord.Message, spacing="\n") -> None:
     fixed_content = find_emoji.sub('', content)
     fixed_content = find_links.sub('', fixed_content)
     fixed_content = find_characters.sub('', fixed_content)
+    fixed_content = find_spoilers.sub('', fixed_content)
 
     len_content = len(fixed_content)
 
     #############################################
     ######### FIND AMOUNT OF CHARACTERS #########
     #############################################
-    doubleyous_search = search("[ｗw]+$", fixed_content)
+    doubleyous_search = search("[w]+$", fixed_content)
     if doubleyous_search:
-        ws = len(search("[ｗw]+$", fixed_content).group())
+        ws = len(search("[w]+$", fixed_content).group())
     else:
         ws = 0
 
@@ -85,13 +94,21 @@ def log(message: discord.Message, spacing="\n") -> None:
     japanese_percent =  100 * jp_ch / len_content
     # non_en_jp_percent = 1 - english_percent - japanese_percent
     # Remove weird characters from channel name
-    channel_name = fix_channel_name(message.channel.name)
     
+    return en_ch, jp_ch, english_percent, japanese_percent
+
+
+
+def log(author: str, channel_name: str, \
+    content: str, en_ch: int, jp_ch: int, \
+    english_percent: int, japanese_percent: int, \
+    spacing="\n") -> None:
 
     ############################
     ######### PRINTING #########
     ############################
-    print(f"Registered {message.author}'s message in {channel_name}: '{content}'")
+    print(spacing)
+    print(f"Registered {author}'s message in {channel_name}: '{content}'")
     msg = f"Len: {len(content)}  EN_CH: {en_ch}   JP_CH: {jp_ch}\n"
     msg += f"JP%: {round(japanese_percent, 2)} EN%: {round(english_percent, 2)}\n"
     msg += ctime()
